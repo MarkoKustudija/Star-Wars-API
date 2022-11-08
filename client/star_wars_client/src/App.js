e8c0769031be5f06646b9d4528fc099b7e61a033
marko <marko@marko.com>
2022-11-08 14:16:16 +0100

    adding fetchMovies() functionality

--- client/star_wars_client/src/App.js
+++ client/star_wars_client/src/App.js
@@ -6,14 +6,14 @@ import MoviesList from "./components/MoviesList";
 function App() {
   const [movies, setMovies] = useState([]);
 
-    // const addMovieHandler = () => {
-    // setMovies((prevValue) => {
-    //   return [...prevValue, newMovie];
-    // });
+  //   const addMovieHandler = (newMovie) => {
+  //   setMovies((prevValue) => {
+  //     return [...prevValue, newMovie];
+  //   });
   // }
 
-  async function addMovieHandler (newMovie) {
-    const response = await fetch('http://localhost:8080/api/movies/', {
+  async function addMovieHandler(newMovie) {
+    const response = await fetch("http://localhost:8080/api/movies/", {
       method: "POST",
       body: JSON.stringify(newMovie),
       headers: {
@@ -22,19 +22,40 @@ function App() {
     });
     const data = await response.json();
     console.log(data);
-  
-  };
-
-  let content = <p>No movie found</p>
-  
-  if(movies.length > 0) {
-    content = <MoviesList  movies = {movies} />
   }
 
+  let content = <p>No movie found</p>;
 
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
 
-  const fetchMoviesHandler = () => {
+  async function fetchMoviesHandler() {
+    const response = await fetch("http://localhost:8080/api/movies/",{
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+    );
+    if (!response.ok) {
+      throw new Error("Somethoing went wrong!");
+    }
+    const data = await response.json();
+    console.log(data);
 
+    const loadedMovies = [];
+
+    for (const key in data) {
+      loadedMovies.push({
+        id: key,
+        title: data[key].title,
+        releaseDate: data[key].releaseDate,
+        openingText: data[key].openingText,
+      });
+    }
+
+    setMovies(loadedMovies);
   }
 
   return (
@@ -45,9 +66,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
